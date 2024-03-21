bba62565011e4c6f348acba68273cfc19f4c9e99
Nando Javier Agüero <nandoaguerojs@gmail.com>
2024-03-21 11:22:48 -0300

    Cambio en Main
    implemente el metodo carga

--- src/tp2/eda/Main.java
+++ src/tp2/eda/Main.java
@@ -14,7 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ordenamiento o1=new Ordenamiento();
+        o1.carga();
     }
     
 }
